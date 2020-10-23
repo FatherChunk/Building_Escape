a84f96a64be69ebe992382608a84d2d02b879178
fatherchunk99 <j_grainger@hotmail.co.uk>
2020-10-23 14:42:19 +0100

    Making Grabber function and printing viewport location and rotation to screen

---
+++ Source/Building_Escape/Grabber.cpp
@@ -0,0 +1,51 @@
+// Copyright James Grainger 2020
+
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+
+#include "Grabber.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Grabber is here!"));
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Get Players viepoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointRotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("Position is: %s, Rotation is %s"), *PlayerViewPointLocation.ToString(), *PlayerViewPointRotation.ToString());
+	
+
+	// Ray Cast to x distance (reach)
+
+	// See what it hits
+}
+
