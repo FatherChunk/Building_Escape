59fd39da95729ef85b26dc24789f97086dacb403
fatherchunk99 <j_grainger@hotmail.co.uk>
2020-10-23 07:50:26 +0100

    Exposing Target Yaw to editor

--- Source/Building_Escape/OpenDoor.cpp
+++ Source/Building_Escape/OpenDoor.cpp
@@ -23,8 +23,8 @@ void UOpenDoor::BeginPlay()
 
 	InitialYaw = GetOwner()->GetActorRotation().Yaw;
 	CurrentYaw = InitialYaw;
-	TargetYaw = InitialYaw + -90.f;
-
+	//TargetYaw = TargetYaw + InitialYaw;
+	TargetYaw += InitialYaw;
 	
 	
 
